24c25e70f700a6aed115dd6acb27e1220aa4c169
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-25 01:39:35 +0900

    #38 Controllers 1

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/create-or-update-user", (req, res) => {
-    res.json({
-        data: "Hey you hit create-or-update-user API endpoint",
-    })
-})
+router.get("/create-or-update-user", myfunction)
 
 
 module.exports = router;
+
+exports.myfunction = (req, res) => {
+    res.json({
+        data: "Hey you hit create-or-update-user API endpoint",
+    })
+}
