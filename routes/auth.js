0fc808b698b7c25f465082d92942d3ac8c4bb04a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-25 02:12:53 +0900

    #41 Middleware 3.2

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,17 @@ const {authCheck} = require('../middlewares/auth')
 // controllers
 const {createOrUpdateUser} = require("../controllers/auth")
 
+// for example
+const myMiddleware = (req, res, next) => {
+    console.log("IM A MIDDLEWARE YAY")
+}
+
 router.post("/create-or-update-user", authCheck, createOrUpdateUser)
+
+router.get("/testing", myMiddleware, (req, res) => {
+    res.json({
+        data: "You successfully tried middleware",
+    })
+})
 
 module.exports = router;
