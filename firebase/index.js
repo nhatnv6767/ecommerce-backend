62676bf3af067d11d7eef4c45cb2c0f37bc92ba0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-25 02:05:59 +0900

    #41 Middleware 1

--- firebase/index.js
+++ firebase/index.js
@@ -1,8 +1,11 @@
 
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
-var serviceAccount = require("../config/fbServiceAccountKey.json");
+const serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
+
+
+module.exports = admin;
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const {createOrUpdateUser} = require("../controllers/auth")
 
-router.get("/create-or-update-user", createOrUpdateUser)
+router.post("/create-or-update-user", createOrUpdateUser)
 
 module.exports = router;
