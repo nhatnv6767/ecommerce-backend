22f9d24922f8b1475da18e92aeaa1cb285a8d744
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-25 22:12:38 +0900

    #43 Create or Update User 2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,7 @@ exports.authCheck = async (req, res, next) => {
     // console.log(req.headers); // token
     try {
         const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken);
+        console.log("FIREBASE USER IN AUTHCHECK", firebaseUser)
     }catch (err) {
         res.status(401).json({
             err: "Invalid or expired token",
