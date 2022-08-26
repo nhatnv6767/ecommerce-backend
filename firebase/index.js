901c849a5a18e660e53960bd8e5d107d968b4b05
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-26 20:54:39 +0900

    #43 Create or Update User 5.1

--- firebase/index.js
+++ firebase/index.js
@@ -4,7 +4,8 @@ const admin = require("firebase-admin");
 const serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://ecommerce-f1952.firebaseio.com",
 });
 
 
