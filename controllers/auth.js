3a702cc7446488a6f31e3254ea5616b390ac11b3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-26 22:20:12 +0900

    #45 Current User Endpoint 2

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,3 +24,12 @@ exports.createOrUpdateUser = async (req, res) => {
         res.json(newUser)
     }
 }
+
+exports.currentUser = async (req, res) => {
+    await User.findOne({
+        email: req.user.email,
+    }).exec((err, user) => {
+        if(err) throw new Error(err)
+        res.json(user)
+    })
+}
